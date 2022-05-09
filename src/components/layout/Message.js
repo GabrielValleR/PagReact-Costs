e0cf609493b0a1cf82aa85fd946d79e74a393358
Gabriel Valle <you@example.comgabrielvallerodrigues@gmail.com>
2022-05-09 10:17:38 -0300

    Finalisado mensagem

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -2,10 +2,31 @@ import { useState, useEffect} from 'react'
 
 import styles from './Message.module.css'
 function Message({type, msg }){
+    const [visible, setVisible] = useState(false)
+
+    useEffect( () => {
+        if(!msg){
+            setVisible(false)
+            return
+        }
+
+        setVisible(true)
+
+        const timer = setTimeout(()=>{
+            setVisible(false)
+        },3000)
+
+        return ()=> clearTimeout(timer)
+    },[msg])
+
     return(
-        <div className={`${styles.message} ${styles[type]}`}>
-            <p>{msg}</p>
-        </div>
+        <>
+           {visible && (
+                <div className={`${styles.message} ${styles[type]}`}>
+                <p>{msg}</p>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,27 @@
+import { useLocation} from 'react-router-dom'
+import Conteiner from "../layout/Conteiner"
+import LinkButton from '../layout/LinkButton'
+
 import Message from "../layout/Message"
 
+import styles from './Projects.module.css'
+
 function Projects(){
+    const location = useLocation()
+    let message =''
+    if(location.state){
+        message = location.state.message
+    }
     return(
-        <div>
-       <h1>Meus Projetos</h1>
-       <Message msg="Alguma mensagem" />
+        <div className={styles.project_container}>
+            <div className={styles.title_cotainer}>
+            <h1>Meus Projetos</h1>
+            <LinkButton to={"/newproject"} text="Criar Novo Projeto"/>
+            </div>
+             {message && <Message type="success" msg={message} />}
+             <Conteiner customClass="start">
+                 <p>Projetos....</p>
+             </Conteiner>
        
         </div>
     )
